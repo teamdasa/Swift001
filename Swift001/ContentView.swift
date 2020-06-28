40cca3a7be3756624a6f14a9c354cc66c1fc7b9f
Amit Gupta <amit@gprof.com>
2020-06-28 10:51:28 -0700

    Initial Commit

---
+++ Swift001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift001
+//
+//  Created by Amit Gupta on 6/28/20.
+//  Copyright © 2020 GProf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
